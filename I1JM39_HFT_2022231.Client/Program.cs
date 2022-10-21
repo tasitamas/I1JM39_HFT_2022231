79a2ccb19f103fb2748f30793a824234b0742bb2
Tasi Tamás <tasitamas1222@gmail.com>
2022-10-21 16:49:25 +0200

    Add project files.

---
+++ I1JM39_HFT_2022231.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace I1JM39_HFT_2022231.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ I1JM39_HFT_2022231.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace I1JM39_HFT_2022231.Models
+{
+    public class Class1
+    {
+    }
+}
