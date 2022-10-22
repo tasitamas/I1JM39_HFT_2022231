ded7c57734f8e0f5a1c18ac0bb6a8371ad0e80dd
Tamás <tasitamas1222@gmail.com>
2022-10-22 23:02:43 +0200

    Generic abstract repository class created

---
+++ I1JM39_HFT_2022231.Repository/GenericRepository/Repository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I1JM39_HFT_2022231.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected GameDbContext ctx;
+        public Repository(GameDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+        public abstract void Update(T item);
+    }
+}
