87c2ff77630a38ce71361b8145aada170b3977ed
Tamás <tasitamas1222@gmail.com>
2022-10-22 23:02:21 +0200

    Generic repository interface created

---
+++ I1JM39_HFT_2022231.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I1JM39_HFT_2022231.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> ReadAll();
+        T Read(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
