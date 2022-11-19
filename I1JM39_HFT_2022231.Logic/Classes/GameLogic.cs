497991c2b406ca4fe85d42194ba7759f9f9f1f92
Tasi Tamás <tasitamas1222@gmail.com>
2022-11-19 20:05:01 +0100

    Logic Non CRUDs fixated

--- I1JM39_HFT_2022231.Logic/Classes/GameLogic.cs
+++ I1JM39_HFT_2022231.Logic/Classes/GameLogic.cs
@@ -1,8 +1,5 @@
 ﻿using I1JM39_HFT_2022231.Models;
 using I1JM39_HFT_2022231.Repository;
-using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
-using Microsoft.EntityFrameworkCore.Storage;
-using Newtonsoft.Json;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -119,149 +116,106 @@ namespace I1JM39_HFT_2022231.Logic
             }
         }
 
-        
-        
+
+
         //Non CRUD Methods
-        public IEnumerable<BasicGameInfo> OldestGameWithDeveloperName()
+        public IEnumerable<object> OldestGameWithDeveloperName()
         {
-            var q = from g in this.gameRepo.ReadAll()
-                    from d in this.devRepo.ReadAll()
-                    let minAge = gameRepo.ReadAll().Min(t => t.Release)
-                    where g.DeveloperId == d.DeveloperId && g.Release == minAge
-                    select new BasicGameInfo()
-                    {
-                       GameName = g.GameName,
-                       DevName = d.DeveloperName,
-                       Age = (int)DateTime.Now.Year - (int)g.Release,
-                    };
-            return q.ToList();
+            var oldest = from x in this.gameRepo.ReadAll()
+                         where x.Release.Equals(gameRepo.ReadAll().Min(t => t.Release))
+                         select new
+                         {
+                             _GameName = x.GameName,
+                             _DevName = x.Developer.DeveloperName,
+                             _Age = DateTime.Now.Year - x.Release,
+                         };
+            return oldest;
         }
-        public IEnumerable<BasicGameInfo> YoungestGameWithDeveloperName()
+        public IEnumerable<object> YoungestGameWithDeveloperName()
         {
-            return from g in this.gameRepo.ReadAll()
-                   from d in this.devRepo.ReadAll()
-                   let maxAge = gameRepo.ReadAll().Max(t => t.Release)
-                   where g.DeveloperId == d.DeveloperId && g.Release == maxAge
-                   select new BasicGameInfo()
-                   {
-                       GameName = g.GameName,
-                       DevName = d.DeveloperName,
-                       Age = (int)DateTime.Now.Year - (int)g.Release,
-                   };
+            var youngest = from x in this.gameRepo.ReadAll()
+                           where x.Release.Equals(gameRepo.ReadAll().Max(t => t.Release))
+                           select new
+                           {
+                               _GameName = x.GameName,
+                               _DevName = x.Developer.DeveloperName,
+                               _Age = DateTime.Now.Year - x.Release,
+                           };
+            return youngest;
         }
-        public IEnumerable<BasicGameInfo> OlderThan10YearsGames()
+
+        public IEnumerable<GameInfo> OlderThan10YearsGames()
         {
-            return from g in this.gameRepo.ReadAll()
-                   from d in this.devRepo.ReadAll()
-                   where g.DeveloperId == d.DeveloperId
-                   && ((int)DateTime.Now.Year - (int)g.Release > 10)
-                   select new BasicGameInfo()
-                   {
-                       DevName = d.DeveloperName,
-                       GameName = g.GameName,
-                       Age = (int)DateTime.Now.Year - (int)g.Release,
-                   };
+            var older = from x in this.gameRepo.ReadAll()
+                        where (DateTime.Now.Year - x.Release) > 10
+                        select new GameInfo()
+                        {
+                            GameName = x.GameName,
+                            DevName = x.Developer.DeveloperName,
+                            Age = DateTime.Now.Year - x.Release,
+                        };
+            return older;
         }
-        public IEnumerable<BasicGameInfo> GamesWithNpc()
+
+        public IEnumerable<KeyValuePair<string, string>> HighestRatingGameWithDevName()
         {
-            var q1 = from g in this.gameRepo.ReadAll()
-                     from c in this.charRepo.ReadAll()
-                     from d in this.devRepo.ReadAll()
-                     where (c.Priority == 3 && c.Priority != 2 && c.Priority != 1) && c.GameId == g.GameId && d.DeveloperId == g.DeveloperId
-                     select new BasicGameInfo()
-                     {
-                         GameName = g.GameName,
-                         DevName = d.DeveloperName,
-                         Age = (int)DateTime.Now.Year - (int)g.Release
-                     };
-            return q1.Distinct();
+            var highest = from x in this.gameRepo.ReadAll()
+                          where x.Rating.Equals(gameRepo.ReadAll().Max(t => t.Rating))
+                          select new KeyValuePair<string, string>
+                          (x.GameName, x.Developer.DeveloperName);
+            return highest;
         }
-        public IEnumerable<RatingInfo> HighestRatingGameWithDevName()
+        public IEnumerable<KeyValuePair<string, string>> LowestRatingGameWithDevName()
         {
-            var q = from g in this.gameRepo.ReadAll()
-                    from d in this.devRepo.ReadAll()
-                    let maxRating = gameRepo.ReadAll().Max(t => t.Rating)
-                    where g.DeveloperId == d.DeveloperId && g.Rating == maxRating
-                    select new RatingInfo()
-                    {
-                       GameName = g.GameName,
-                       DevName = d.DeveloperName,
-                       Rating = g.Rating,
-                       Price = g.Price,
-                    };
-            return q.ToList();
+            var lowest = from x in this.gameRepo.ReadAll()
+                         where x.Rating.Equals(gameRepo.ReadAll().Min(t => t.Rating))
+                         select new KeyValuePair<string, string>
+                         (x.GameName, x.Developer.DeveloperName);
+            return lowest;
         }
-        public IEnumerable<RatingInfo> LowestRatingGameWithDevName()
-        {
-            return from g in this.gameRepo.ReadAll()
-                   from d in this.devRepo.ReadAll()
-                   let minRating = gameRepo.ReadAll().Min(t => t.Rating)
-                   where g.DeveloperId == d.DeveloperId && g.Rating == minRating
-                   select new RatingInfo()
-                   {
-                       GameName = g.GameName,
-                       DevName = d.DeveloperName,
-                       Rating = g.Rating,
-                       Price = g.Price,
-                   };
-        }
+
         public IEnumerable<RatingInfo> FreeGames()
         {
-            return from g in this.gameRepo.ReadAll()
-                   from d in this.devRepo.ReadAll()
-                   where g.DeveloperId == d.DeveloperId && g.Price == 0
-                   select new RatingInfo()
-                   {
-                       GameName = g.GameName,
-                       DevName = d.DeveloperName,
-                       Rating = g.Rating,
-                       Price = g.Price,
-                   };
+            var free = from x in this.gameRepo.ReadAll()
+                       where x.Price == 0
+                       select new RatingInfo()
+                       {
+                           GameName = x.GameName,
+                           DevName = x.Developer.DeveloperName,
+                           Rating = x.Rating
+                       };
+            return free;
         }
         public IEnumerable<RatingInfo> PaidGames()
         {
-            return from g in this.gameRepo.ReadAll()
-                   from d in this.devRepo.ReadAll()
-                   where g.DeveloperId == d.DeveloperId && g.Price > 0
-                   select new RatingInfo()
-                   {
-                       GameName = g.GameName,
-                       DevName = d.DeveloperName,
-                       Rating = g.Rating,
-                       Price= g.Price,
-                   };
+            var paid = from x in this.gameRepo.ReadAll()
+                       where x.Price > 0
+                       select new RatingInfo()
+                       {
+                           GameName = x.GameName,
+                           DevName = x.Developer.DeveloperName,
+                           Rating = x.Rating
+                       };
+            return paid;
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> GamesCharactersCount()
+        {
+            var count = from x in charRepo.ReadAll()
+                        group x by x.Game.GameName into g
+                        orderby g.Count() descending
+                        select new KeyValuePair<string, int>
+                        (g.Key, g.Count());
+            return count;
         }
     }
 
     //Helping classes
-    public class BasicGameInfo
+    public class RatingInfo
     {
         public string GameName { get; set; }
         public string DevName { get; set; }
-        public int Age { get; set; }
-        public override bool Equals(object obj)
-        {
-            BasicGameInfo g = obj as BasicGameInfo;
-            if (g == null)
-            {
-                return false;
-            }
-            else
-            {
-                return this.GameName == g.GameName && this.DevName == g.DevName && this.Age == g.Age;
-            }
-        }
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(this.GameName, this.DevName, this.Age);
-        }
-    }
-    public class RatingInfo
-    { 
-        public string GameName { get; set; }
-        public string DevName { get; set; }
         public double Rating { get; set; }
-        public double Price { get; set; }
 
         public override bool Equals(object obj)
         {
@@ -271,14 +225,37 @@ namespace I1JM39_HFT_2022231.Logic
                 return false;
             }
             else
-            { 
-            return this.GameName == r.GameName && this.DevName == r.DevName && this.Rating == r.Rating && this.Price == r.Price;
+            {
+                return this.GameName == r.GameName && this.DevName == r.DevName && this.Rating == r.Rating;
             }
         }
         public override int GetHashCode()
         {
-            return HashCode.Combine(this.GameName, this.DevName,this.Rating,this.Price);
+            return HashCode.Combine(this.GameName, this.DevName, this.Rating);
         }
     }
-    
+    public class GameInfo
+    {
+        public string GameName { get; set; }
+        public string DevName { get; set; }
+        public int Age { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            GameInfo r = obj as GameInfo;
+            if (r == null)
+            {
+                return false;
+            }
+            else
+            {
+                return this.GameName == r.GameName && this.DevName == r.DevName && this.Age == r.Age;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.GameName, this.DevName, this.Age);
+        }
+    }
+
 }
--- I1JM39_HFT_2022231.Logic/Interfaces/IGameLogic.cs
+++ I1JM39_HFT_2022231.Logic/Interfaces/IGameLogic.cs
@@ -15,13 +15,13 @@ namespace I1JM39_HFT_2022231.Logic
         void Update(Game item);
 
         //Non CRUD Methods
-        public IEnumerable<BasicGameInfo> OldestGameWithDeveloperName();
-        public IEnumerable<BasicGameInfo> YoungestGameWithDeveloperName();
-        public IEnumerable<BasicGameInfo> OlderThan10YearsGames();
-        public IEnumerable<BasicGameInfo> GamesWithNpc();
-        public IEnumerable<RatingInfo> HighestRatingGameWithDevName();
-        public IEnumerable<RatingInfo> LowestRatingGameWithDevName();
+        public IEnumerable<object> OldestGameWithDeveloperName();
+        public IEnumerable<object> YoungestGameWithDeveloperName();
+        public IEnumerable<GameInfo> OlderThan10YearsGames();
+        public IEnumerable<KeyValuePair<string, string>> HighestRatingGameWithDevName();
+        public IEnumerable<KeyValuePair<string, string>> LowestRatingGameWithDevName();
         public IEnumerable<RatingInfo> FreeGames();
         public IEnumerable<RatingInfo> PaidGames();
+        public IEnumerable<KeyValuePair<string, int>> GamesCharactersCount();
     }
 }
