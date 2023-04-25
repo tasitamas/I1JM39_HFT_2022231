8ca79c7160fec9cdf1c0136840b61db36f4a097f
Tasi Tamás <tasitamas@stud.uni-obuda.hu>
2023-04-25 15:24:42 +0200

    Modified the MainWindow, added MWVM.cs

---
+++ I1JM39_GUI_2022232.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I1JM39_GUI_2022232.WpfClient
+{
+    public class MainWindowViewModel
+    {
+    }
+}
